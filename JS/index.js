5c2821b826aa5063e60bee69a6cb424eb3d9a87a
kunal <kunalkakati2003@gmail.com>
2022-06-21 20:33:04 +0530

    Change my age using JS

--- JS/index.js
+++ JS/index.js
@@ -3,6 +3,7 @@ const icon = document.querySelector(".hum__btn i");
 const navLinks = document.querySelector(".links");
 const links = document.querySelectorAll(".links li");
 const copyText = document.querySelector(".gmail");
+const age = document.getElementById("my_age");
 
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("open");
@@ -10,6 +11,9 @@ hamburger.addEventListener("click", () => {
     icon.classList.toggle("fa-times");
   },500);
 });
+
+const year = new Date().getFullYear();
+age.innerText = year - 2001;
 
 
 
